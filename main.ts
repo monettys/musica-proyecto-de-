0b6dc8faeda531d14b16c4d4f61a720613f1cb4f
monettys <124165786+monettys@users.noreply.github.com>
2023-02-01 14:35:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(147, music.beat(BeatFraction.Whole))
+    music.playTone(147, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
